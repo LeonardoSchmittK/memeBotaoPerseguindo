f8235391a3a214db5f994aba1e39ce33057c2280
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-08 20:13:28 -0300

    feat:arrange code

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
 const noBtn = document.querySelector(".nao");
 const yesBtn = document.querySelector(".sim");
-let sizeWidthSim = Number.parseInt(
-  window.getComputedStyle(document.querySelector(".sim")).width
-);
-let sizeHeightSim = Number.parseInt(
-  window.getComputedStyle(document.querySelector(".sim")).height
-);
-let sizeFontSim = Number.parseInt(
-  window.getComputedStyle(document.querySelector(".sim")).fontSize
-);
 
+const quotes = [
+  "Vai, aperta o botão",
+  "Por favorzinho...",
+  "Confia, a gente combina",
+  "Acredite em nós!",
+  "Doeu? Quando caiu do céu, meu anjo?",
+  "Eu compro flores, acredita?",
+];
+
+// pegar as dimensoes da tela
+let x = window.innerWidth;
+let y = window.innerHeight;
+
+let jaclicou = false;
+
+// botao nao é clicado
 function handleClick() {
-  sizeHeightSim += 210;
-  sizeWidthSim += 220;
-  sizeFontSim += 120;
+  printRandomQuote();
+  jaclicou = true;
 
-  const randomX = Math.floor(Math.random() * window.screen.availWidth);
-  const randomY = Math.floor(Math.random() * window.screen.availHeight);
+  // randomizar onde o botao nao aparecerá
+  let randomX = Math.floor(Math.random() * x);
+  let randomY = Math.floor(Math.random() * y);
+
+  // Definindo os limites para garantir que o botão esteja dentro da visualização
+  const buttonWidth = noBtn.offsetWidth;
+  const buttonHeight = noBtn.offsetHeight;
+
+  // Verificar se a posição aleatória fica fora da tela
+  if (randomX + buttonWidth > x) {
+    randomX = x - buttonWidth;
+  }
+  if (randomY + buttonHeight > y) {
+    randomY = y - buttonHeight;
+  }
 
   noBtn.style.position = "absolute";
   noBtn.style.left = `${randomX}px`;
   noBtn.style.top = `${randomY}px`;
-
-  console.log(randomX);
-  console.log(randomY);
-
-  yesBtn.style.width = `${sizeWidthSim}px`;
-  yesBtn.style.height = `${sizeHeightSim}px`;
-  yesBtn.style.fontSize = `${sizeFontSim}px`;
 }
 
+// sortear frase
+
+function printRandomQuote() {
+  const randomNum = Math.floor(Math.random() * quotes.length);
+  document.querySelector(".title").innerText = quotes[randomNum];
+}
+
+// botao sim é clicado
 function handleYes() {
+  // criar video e botar no body
   const video = document.createElement("video");
   video.src = "./meme.mp4";
-  video.autoplay = true;
+  video.autoplay = true; // comeca sozinho
   video.loop = true;
   document.body.appendChild(video);
+
+  // deixa so o video na tela
   document.querySelector(".container").style.display = "none";
   document.body.style.background = "black";
 
+  // loop a cada 4s
   setInterval(() => {
     handleYes();
     document.body.removeChild(document.querySelector("video"));
   }, 4000);
 }
+
+// quando a tela muda da tamanho, pegue as dimensões de novo
+window.onresize = () => {
+  if (jaclicou) {
+    handleClick();
+
+    x = window.innerWidth;
+    y = window.innerHeight;
+  }
+};
